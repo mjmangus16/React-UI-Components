35c7549e1b9a82611bcf96c995dee16fd25d4d5d
MMangus <mjmangus16@yahoo.com>
2019-05-28 10:37:14 -0400

    completed MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+import HeaderContent from "./HeaderContent";
+import HeaderTitle from "./HeaderTitle";
+import ImageThumbnail from "./ImageThumbnail";
+
+const HeaderContainer = () => {
+  return (
+    <div className="header-container">
+      <ImageThumbnail />
+      <div>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,37 @@
-import React from 'react';
-import './Header.css';
+import React, { Fragment } from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const date = new Date();
+
+  let today = (
+    <p>
+      {date.getDate()} {months[date.getMonth()]}
+    </p>
+  );
+
+  return (
+    <div className="timestamp-container">
+      <h3>Lambda School</h3>
+      <p>@LambdaSchool</p>
+      {today}
+    </div>
+  );
+};
+
+export default HeaderTitle;
